f3aa768bf80922867e6a7a6f9fcd302ef98fbfd8
preich21 <paddi1113@gmail.com>
2025-05-18 22:11:23 +0200

    world-drive: add google maps integration

---
+++ frontend/pse-cars/app/world-drive/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import GoogleMapsComponent from "@/app/world-drive/GoogleMapsComponent"
+
+export default function WorldDrive() {
+    return (
+        <>
+            <div className="mt-20"></div>
+            <GoogleMapsComponent/>
+        </>
+    )
+}
