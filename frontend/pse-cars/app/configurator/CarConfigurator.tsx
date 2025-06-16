88873ff528d00df8d7d725e03fa88ed80b89548f
Axel Herrmann <axel-herrmann@gmx.net>
2025-06-16 23:39:16 +0200

    fix hash parsing bug

--- frontend/pse-cars/app/configurator/CarConfigurator.tsx
+++ frontend/pse-cars/app/configurator/CarConfigurator.tsx
@@ -5,27 +5,6 @@ import Button from "@/app/components/Button";
 import React, {useEffect, useState} from "react";
 import {CarFeature} from "@/app/http-client/openapi";
 
-function parseHash(features: StateFulCarFeature[]) {
-  const hash = window.location.hash.slice(1); // remove '#'
-  const params = new URLSearchParams(hash);
-  for (let feature of features) {
-    const hashValue = params.get(feature.name!);
-    if (hashValue) feature.setVariant(hashValue);
-  }
-  return {
-    activeTabId: Number(params.get("tab"))
-  };
-}
-
-function updateHash(features: StateFulCarFeature[], activeTabId?: number) {
-  const params = new URLSearchParams();
-  for (let feature of features) {
-    if (feature.variant) params.set(feature.name!, feature.variant);
-  }
-  if (activeTabId) params.set("tab", String(activeTabId));
-  window.location.hash = params.toString();
-}
-
 interface StateFulCarFeature extends CarFeature {
   variant: string;
   setVariant: (variant: string) => void;
@@ -39,33 +18,32 @@ export function CarConfigurator({
   const statefulFeatures = features.map(feature => {
     const [featureState, setFeatureState] = useState(feature.variants![0]);
     (feature as StateFulCarFeature).variant = featureState;
-    (feature as StateFulCarFeature).setVariant = setFeatureState;
+    (feature as StateFulCarFeature).setVariant = (newState) => {
+      setFeatureState(newState);
+    };
     return feature;
   }) as StateFulCarFeature[];
   const [activeTab, setActiveTab] = React.useState(statefulFeatures[0]);
 
   useEffect(() => {
-    const { activeTabId } = parseHash(statefulFeatures);
-    const newTab = statefulFeatures.find(feature => feature.id === activeTabId);
+    const hash = window.location.hash.slice(1); // remove '#'
+    const params = new URLSearchParams(hash);
+    for (let feature of statefulFeatures) {
+      const hashValue = params.get(feature.name!);
+      if (hashValue) feature.setVariant(hashValue);
+    }
+    const newTab = statefulFeatures.find(feature => feature.id === Number(params.get("tab")));
     if (newTab) setActiveTab(newTab);
   }, []);
 
-  // TODO: fix that hash is set to default values on first render
-
   useEffect(() => {
-    updateHash(statefulFeatures, activeTab.id);
+    const params = new URLSearchParams();
+    for (let feature of statefulFeatures) {
+      if (feature.variant) params.set(feature.name!, feature.variant);
+    }
+    if (activeTab.id) params.set("tab", String(activeTab.id));
+    window.location.hash = params.toString();
   }, [...statefulFeatures.map(feature => feature.variant), activeTab]);
-
-
-  useEffect(() => {
-    const onHashChange = () => {
-      const { activeTabId } = parseHash(statefulFeatures);
-      const newTab = statefulFeatures.find(feature => feature.id === activeTabId);
-      if (newTab) setActiveTab(newTab);
-    };
-    window.addEventListener("hashchange", onHashChange);
-    return () => window.removeEventListener("hashchange", onHashChange);
-  }, []);
 
   const saveConfiguration = () => {
     // openApiClient!.saveCar({}, {
